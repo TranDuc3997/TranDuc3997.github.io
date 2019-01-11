531e6c6fe980d32e6725e21be2b11d255dd7e998
TranDuc3997 <vanduc3997@gmail.com>
2019-01-11 16:30:37 +0700

    updae Jquery 4

--- JQuery/bai4/js/scripts.js
+++ JQuery/bai4/js/scripts.js
@@ -1,41 +1,25 @@
-var rotation = [1080,-720,-360,360,720,1080];
-$(document).ready(function() {
-});
-var id = 1;
-function createLeaves(id) {
-  idleaves = generateRandomNumber(1,5);
-  var img =  $('<img />', {
-            id: 'leaves-'+ id +'-js',
-            class: 'leaves--hidden',
-            src: 'images/leaves'+ idleaves +'.png',
-            alt: 'leaves1'
-            }).appendTo('.container');
-    return img;
-}
-function generateRandomNumber(min , max) {
-    return Math.floor(Math.random() * (max-min) + min );
-} 
-function removeLeaves (leaves) {
-    $(leaves).remove();
-}
-setInterval(function(){
-    var leaves = createLeaves(id++);
-    var time = generateRandomNumber(5,15);
-    var maxWidth =Math.floor(($(document).width()/2) / 10);
-    var x_start = generateRandomNumber(-maxWidth,maxWidth) * 10;
-    var x_end = generateRandomNumber(-maxWidth,maxWidth) * 10;
-    var y_end = 620;
-    var rotaX = rotation[generateRandomNumber(0,5)];
-   TweenMax.fromTo($(leaves), time, {
-       x: x_start,
-       y: 0
-    },{
-       x: x_end,
-       y: y_end,
-       rotationX:rotation[generateRandomNumber(0,5)],
-       rotationY:rotation[generateRandomNumber(0,5)],
-       onComplete: function () {
-        removeLeaves(leaves);
-       }
+var mouse_x = -1 ;
+$(document).ready(function(){
+    $(document).mousemove(function(event){
+      $("span").text(event.pageX + ", " + event.pageY);
+      if(mouse_x != -1) {
+          if(mouse_x < event.pageX){
+              moveLayer(4,1);
+              moveLayer(2,0.5);
+              moveLayer(3,0.75);
+          }
+          else{
+            moveLayer(4,-1);
+            moveLayer(2,-0.5);
+            moveLayer(3,-0.75);
+          }
+          mouse_x = event.pageX;
+      }
+      else{
+          mouse_x = event.pageX;
+      }
     });
-},700);
+});
+function moveLayer(idLayer,speed){
+    $("#layer-"+idLayer).css({left: '+=' + speed});
+}
