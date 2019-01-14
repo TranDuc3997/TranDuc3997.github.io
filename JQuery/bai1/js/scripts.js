0a1db92a2c22d4d33966e3cdce157e19c8002ad7
TranDuc3997 <vanduc3997@gmail.com>
2019-01-14 10:23:30 +0700

    update JQuery1

--- JQuery/bai1/js/scripts.js
+++ JQuery/bai1/js/scripts.js
@@ -1,8 +1,10 @@
-var index_prev = 0
+var index_prev = 0 // index click prev
+// event click bar
 $(function(){
-  $(".button").click(function() {
+  $(".button-js").click(function() {
     var sliderSplit = this.id.split('-');
     var index = parseInt(sliderSplit[1]);
+    $(".btn__hidden").css('pointer-events', 'none'); // disable event click
     if(index == 1){
       $("#modal-1").css('display','block').css('top','8px').animate({top:60},500);
     }
@@ -11,22 +13,24 @@ $(function(){
     }
   });
 });
-
+//event close modal
 $(function(){
-  $(".close").click(function(){
+  $(".close-js").click(function(){
+    $(".btn__hidden").css('pointer-events','auto'); // enable event click
     var sliderSplit = this.id.split('-');
-    $("#modal-"+sliderSplit[1]).css("display", "none");
+    $("#modal-"+sliderSplit[1]).css('display', 'none');
+
   });
 });
-
+//event handling index bar
 $(function() {
-  $('.btn__hidden').click(function() {
+  $(".btn__hidden").click(function() {
     var sliderSplit = this.id.split('-'); // split the string at the hyphen
     var index = parseInt(sliderSplit[1]);
     var src;
     if($(this).attr('src') === 'images/about'+ index +'_mb_hover.jpg'){
       src = 'images/about'+ index +'_mb.jpg';
-      $('#hd' + index).animate({
+      $('#hd' + index + '-js').animate({
         height: 'toggle'
       });
       index_prev = 0;
@@ -35,9 +39,9 @@ $(function() {
     else{
       if(index_prev > 0){
         $('#about-' + index_prev).attr('src','images/about'+ index_prev +'_mb.jpg');
-        $('#hd' + index_prev).hide();
+        $('#hd' + index_prev + '-js').hide();
       }
-      $('#hd' + index).animate({
+      $('#hd' + index + '-js').animate({
         height: 'toggle'
       });
       src = 'images/about'+ index +'_mb_hover.jpg';
