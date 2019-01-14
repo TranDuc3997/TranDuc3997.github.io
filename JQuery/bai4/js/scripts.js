b6405dd3facb83e60c4f06695fbee776218af59b
TranDuc3997 <vanduc3997@gmail.com>
2019-01-14 11:34:54 +0700

    update HTML 2

--- JQuery/bai4/js/scripts.js
+++ JQuery/bai4/js/scripts.js
@@ -1,22 +1,28 @@
-var mouse_x = -1 ;
+var mouse_x = -1 ; // initialize mouse_x
+
 $(document).mousemove(function(event){
   if(mouse_x != -1) {
-      if(mouse_x < event.pageX){
-          moveLayer(4,1);
+      if(mouse_x < event.pageX){ // layers move to right display
+          moveLayer(4,1); 
           moveLayer(2,0.5);
           moveLayer(3,0.75);
       }
-      else{
+      else{ // layers move to left display
         moveLayer(4,-1);
         moveLayer(2,-0.5);
         moveLayer(3,-0.75);
       }
-      mouse_x = event.pageX;
+      mouse_x = event.pageX; // assign mouse_x = value mouse present
   }
   else{
-      mouse_x = event.pageX;
+      mouse_x = event.pageX; // assign mouse_x = value mouse present
   }
 });
+/**
+ * function movelayer with event move mouse
+ * @param {id layer} idLayer 
+ * @param {speed move layer} speed 
+ */
 function moveLayer(idLayer,speed){
     $("#layer-"+idLayer).css({left: '+=' + speed});
 }
