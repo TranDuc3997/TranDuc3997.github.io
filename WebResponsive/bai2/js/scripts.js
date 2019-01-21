ff32fd64a155ffc79ee6e05d3431092c8c703dd3
TranDuc3997 <vanduc3997@gmail.com>
2019-01-21 10:26:30 +0700

    update RP 2

---
+++ WebResponsive/bai2/js/scripts.js
@@ -0,0 +1,39 @@
+var flag = false;
+$(window).scroll(function() {
+if(Math.floor($(window).scrollTop() + $(window).height()) >= $(document).height() - 1) {
+    $(".blood-js").show();
+    $(".row").children("p").css("color","#ffffff");
+    $(".content").css("background-color","#000000").css("color","#840000");
+    $(".info").css("background-color","#330000").css("color","#850000");
+    $(".header__body__content").css("background-color","#330000").css("color","#867d7d").attr("src","images/header__banner--info-hornor.png");
+    $("h3").css("border-color","#330000");
+    changeImage(true);
+    $( document).css("pointer-events","none");
+    $("html, body").animate({
+        scrollTop: "0px",
+    }, 5000);
+    setTimeout(function(){
+        changeImage(false);
+        $(".blood-js").hide();
+        $(".row").children("p").removeAttr("style");
+        $(".content").removeAttr("style");
+        $(".info").removeAttr("style");
+        $(".header__body__content").removeAttr("style");
+    },6000);
+}
+
+});
+ // Change Image follow type
+ function changeImage (typeImage) {
+    if(typeImage)
+        $(".row").each(function(index) {
+            iImage = index + 1;
+           $(this).children("img").attr("src","images/content__img-"+ iImage +"_hornor.jpg");
+    });
+    else {
+        $(".row").each(function(index) {
+            iImage = index + 1;
+           $(this).children("img").attr("src","images/content__img-"+ iImage +".jpg");
+        });
+    }
+ }
